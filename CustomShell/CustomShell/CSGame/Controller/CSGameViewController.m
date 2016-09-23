0defdf994e6cbf0c9022ede7d09a28584c95487d
yi.guo <yi.guo@busonline.com>
2016-09-23 18:57:11 +0800

    小修改

--- CustomShell/CustomShell/CSGame/Controller/CSGameViewController.m
+++ CustomShell/CustomShell/CSGame/Controller/CSGameViewController.m
@@ -16,6 +16,7 @@
     //  Scene
     SCNScene *_scene;
     SCNView *_gameView;
+    SCNNode *_cameraNode;
 }
 @end
 @implementation CSGameViewController
@@ -26,7 +27,7 @@
     [self _prepareScene];
     [self _prepareFloor];
     [self _prepareWall];
-    [self _prepareOtherNode];
+//    [self _prepareOtherNode];
 //    [self _prepareCubeNode];
 }
 
@@ -35,11 +36,11 @@
 - (void)_prepareScene {
     _scene = [SCNScene scene];
     
-    SCNNode *cameraNode = [SCNNode node];
-    cameraNode.camera = [SCNCamera camera];
-    cameraNode.position = SCNVector3Make(0, 1000, 1000);
-    cameraNode.camera.zFar = 4000;
-    [_scene.rootNode addChildNode:cameraNode];
+    _cameraNode = [SCNNode node];
+    _cameraNode.camera = [SCNCamera camera];
+    _cameraNode.position = SCNVector3Make(0, 170, 10);
+    _cameraNode.camera.zFar = 4000;
+    [_scene.rootNode addChildNode:_cameraNode];
     
     SCNNode *lightNode = [SCNNode node];
     lightNode.light = [SCNLight light];
@@ -57,7 +58,7 @@
     _gameView.scene = _scene;
     _gameView.backgroundColor = [UIColor blackColor];
     _gameView.showsStatistics = YES;
-    _gameView.allowsCameraControl = YES;
+//    _gameView.allowsCameraControl = YES;
     [self.view addSubview:_gameView];
 }
 
@@ -81,33 +82,83 @@
     
     float height = 280;
     float depth = 36;
+    float width = 808;
+    float length = 1223;
     
-    SCNBox *wall = [SCNBox boxWithWidth:808
+    //      outside wall
+    
+    //  北墙
+    SCNBox *wall1 = [SCNBox boxWithWidth:width
                                  height:height
                                  length:depth
                           chamferRadius:0];
-    SCNNode *wallNode = [SCNNode nodeWithGeometry:wall];
-    wallNode.position = SCNVector3Make(0, height/2.0, -1223/2.0+depth/2.0);
-    [_scene.rootNode addChildNode:wallNode];
+    SCNNode *wall1Node = [SCNNode nodeWithGeometry:wall1];
+    wall1Node.position = SCNVector3Make(0, height/2.0, -length/2.0+depth/2.0);
+    [_scene.rootNode addChildNode:wall1Node];
     
+    //  东墙
     SCNBox *wall2 = [SCNBox boxWithWidth:depth
                                   height:height
-                                  length:1223
+                                  length:length
                            chamferRadius:0];
     SCNNode *wall2Nodel = [SCNNode nodeWithGeometry:wall2];
-    wall2Nodel.position = SCNVector3Make(404-depth/2.0, height/2.0f, 0);
+    wall2Nodel.position = SCNVector3Make(width/2.0-depth/2.0, height/2.0f, 0);
     [_scene.rootNode addChildNode:wall2Nodel];
     
+    //  主卧阳台 南墙
     SCNBox *wall3 = [SCNBox boxWithWidth:368
                                     height:height
                                     length:depth
                              chamferRadius:0];
     SCNNode *wall3Node = [SCNNode nodeWithGeometry:wall3];
-    wall3Node.position = SCNVector3Make(220, height/2.0, 1223/2.0-depth/2.0);
+    wall3Node.position = SCNVector3Make((width-wall3.width)/2.0, height/2.0, length/2.0-depth/2.0);
     [_scene.rootNode addChildNode:wall3Node];
+    
+    //  主卧西墙
+    SCNBox *wall4 = [SCNBox boxWithWidth:depth
+                                  height:height
+                                  length:486
+                           chamferRadius:0];
+    SCNNode *wall4Node = [SCNNode nodeWithGeometry:wall4];
+    wall4Node.position = SCNVector3Make(wall1.width/2.0-wall3.width+depth/2.0, height/2.0, wall2.length/2.0-wall4.length/2.0);
+    [_scene.rootNode addChildNode:wall4Node];
+    
+    //  卫生间南墙
+    SCNBox *wall5 = [SCNBox boxWithWidth:width-wall3.width
+                                  height:height
+                                  length:depth
+                           chamferRadius:0];
+    SCNNode *wall5Node = [SCNNode nodeWithGeometry:wall5];
+    wall5Node.position = SCNVector3Make(-(width-wall5.width)/2.0, height/2.0, length/2.0-wall4.length+depth/2.0);
+    [_scene.rootNode addChildNode:wall5Node];
+    
+    //  厕所西墙
+    SCNBox *wall6 = [SCNBox boxWithWidth:depth
+                                  height:height
+                                  length:276
+                           chamferRadius:0];
+    SCNNode *wall6Node = [SCNNode nodeWithGeometry:wall6];
+    wall6Node.position = SCNVector3Make(-width/2.0+60, height/2.0, length/2.0-wall4.length-wall6.length/2.0);
+    [_scene.rootNode addChildNode:wall6Node];
+    
+    //  门厅南墙
+    SCNBox *wall7 = [SCNBox boxWithWidth:140 height:height length:depth chamferRadius:0];
+    SCNNode *wall7Node = [SCNNode nodeWithGeometry:wall7];
+    wall7Node.position = SCNVector3Make(-width/2.0+60, height/2.0, length/2.0-wall4.length-wall6.length+depth/2.0);
+    [_scene.rootNode addChildNode:wall7Node];
+    
+    //  书房西墙
+    SCNBox *wall8 = [SCNBox boxWithWidth:depth height:height length:330 chamferRadius:0];
+    SCNNode *wall8Node = [SCNNode nodeWithGeometry:wall8];
+    wall8Node.position = SCNVector3Make(-width/2.0, height/2.0, -(length-wall8.length)/2.0);
+    [_scene.rootNode addChildNode:wall8Node];
+    
+    
+    //      inside wall
     
     
 }
+
 
 - (void)_prepareOtherNode {
     SCNSphere *sun = [SCNSphere sphereWithRadius:2];
@@ -150,7 +201,7 @@
 }
 
 #pragma mark - Touch - Delegate
-/*
+
 - (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event {
     _touchBeganPoint = [touches.anyObject locationInView:self.view];
 }
@@ -158,10 +209,11 @@
 - (void)touchesMoved:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event {
     CGPoint point = [touches.anyObject locationInView:self.view];
     
-    float diffX = fabs(point.x - _touchBeganPoint.x);
-    float diffY = fabs(point.y - _touchBeganPoint.y);
-    BOOL horizontal = diffX > diffY;
+    float diffX = point.x - _touchBeganPoint.x;
+    float diffY = point.y - _touchBeganPoint.y;
     
+    /*
+     BOOL horizontal = diffX > diffY;
     NSArray *hitResults = [_gameView hitTest:point options:nil];
     
     if (!hitResults.count) {
@@ -182,7 +234,9 @@
         }
         result.node.rotation = SCNVector4Make(1, 0, 0, result.node.rotation.w+w_value);
     }
+    */
     
+    _cameraNode.position = SCNVector3Make(diffX, _cameraNode.position.y, diffY);
 }
-*/
+
 @end
